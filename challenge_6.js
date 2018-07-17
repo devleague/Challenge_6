fbe17b991379309c540db23bf020e7c8bda4e207
Bronson Avila <bronson.avila@gmail.com>
2018-07-17 09:40:40 -1000

    Complete challenge

--- challenge_6.js
+++ challenge_6.js
@@ -6,7 +6,7 @@
  */
 exports.SumOfASquare = SumOfASquare;
 
-function SumOfASquare(naturalNumbers){
+function SumOfASquare(naturalNumbers) {
   this.naturalNumbers = naturalNumbers;
 }
 
@@ -14,3 +14,30 @@ function SumOfASquare(naturalNumbers){
 
 // overwrite the object prototype, or add methods to the prototype
 // to satisfy the two method calls, sumOfSquares() and squareOfTheSums()
+
+SumOfASquare.prototype.createNumbersArray = function() {
+  const numbersArray = [];
+
+  for (let i = this.naturalNumbers; i > 0; i--) {
+    numbersArray.push(i);
+  }
+
+  return numbersArray;
+};
+
+SumOfASquare.prototype.sumOfSquares = function() {
+  const numbersArray = this.createNumbersArray();
+
+  return numbersArray
+    .map(num => Math.pow(num, 2))
+    .reduce((sum, currentValue) => sum + currentValue);
+};
+
+SumOfASquare.prototype.squareOfTheSums = function() {
+  const numbersArray = this.createNumbersArray();
+
+  return Math.pow(
+    numbersArray.reduce((sum, currentValue) => sum + currentValue),
+    2
+  );
+};
